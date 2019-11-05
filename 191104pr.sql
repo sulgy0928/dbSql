b6200b42b8559667a39992bb3faa4f822689080f
whji <sulgy0928@gmail.com>
2019-11-05 21:35:52 +0900

    practice

--- 191104pr.sql
+++ 191104pr.sql
@@ -40,6 +40,124 @@ SELECT ROWNUM, a.*
     FROM emp e
     ORDER BY ename)a;
 
+--년 월 파라미터가 주어졌을 때 해당 년 월의 일 수 구하기.
+
+
+desc emp;
+
+SELECT empno, ename, sal, comm, coalesce(comm,sal)
+FROM emp;
+
+SELECT empno, ename, job, sal, 
+             case 
+                    when job = 'SALESMAN' then sal *1.05
+                    when job = 'MANAGER' then sal *1.10
+                    when job = 'PRESIDENT' then sal *1.20
+                    else sal
+             end case_sal
+FROM emp;
+
+SELECT userid, usernm, reg_dt,
+            NVL(reg_dt, SYSDATE) n_reg_dt
+FROM users;
+
+SELECT empno,ename,
+               DECODE( deptno, 10, 'ACCOUNTING',
+                                  20,  'RESEARCH',
+                                 30, 'SALES',
+                                40, 'OPERATIONS','DDIT')dname
+FROM emp;
+
+SELECT empno, ename, hiredate, 
+        case
+                    when MOD(TO_CHAR(hiredate,'YY'),2)=0  then'건강검진 비대상자'
+                    when MOD(TO_CHAR(hiredate,'YY'),2)=1 then '건강검진 대상자'
+        end see_a_doctor
+FROM emp;
+
+SELECT userid, usernm,alias, reg_dt,
+                case
+                            when MOD(TO_CHAR(reg_dt,'YY'),2)=0 then '건강검진 비대상자'
+                            when MOD(TO_CHAR(reg_dt,'YY'),2)=1 then '건강검진 대상자'
+                end
+FROM users;
+
+--그룹함수( AVG, MAX, MIN, SUM, COUNT)
+--그룹함수는 NULL값을 계산대상에서 제외한다.
+--SUM(comm), COUNT(*), COUNT(mgr)
+--직원 중 가장 높은 급여를 받는 사람 조회하기.
+--직원중에서 가장 낮은 급여를 받는 사람 조회.
+--직원의 급여평균(소숫점 둘째자리까지만 나오게. 셋째자리에서 반올림)
+--직원의 급여 전체 합
+--직원의 수
+
+--부서별 최대급여
+SELECT deptno, MAX(sal) max_sal
+FROM emp
+GROUP BY deptno
+HAVING MAX(sal) > 3000;
+
+SELECT deptno, MAX(sal)max_sal
+FROM emp
+GROUP BY deptno
+HAVING MAX(sal) > 3000;
+
+--실습grp1 ppt 159p
+SELECT MAX(sal) max_sal,
+        MIN(sal) min_sal,
+        ROUND(AVG(sal),2) avg_sal,
+        SUM(sal) sum_sal,
+        COUNT(sal) count_sal,
+        COUNT(mgr) count_mgr,
+        COUNT(*) count_all
+FROM emp;
+
+SELECT MAX(sal) max_sal,
+              MIN(sal),
+              ROUND(AVG(sal),2),
+              SUM(sal),
+              COUNT(sal),
+              COUNT(mgr),
+              COUNT(*)
+FROM emp;
+    
+              
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
